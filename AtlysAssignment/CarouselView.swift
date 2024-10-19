465652bfc598e0e1f197662ae1c7103f4b555448
Harshvardhan Sharma <78471522+harshvs7@users.noreply.github.com>
2024-10-19 10:54:27 +0530

    Image Caraousel

---
+++ AtlysAssignment/CarouselView.swift
@@ -0,0 +1,20 @@
+//
+//  CarouselView.swift
+//  AtlysAssignment
+//
+//  Created by Harshvardhan Sharma on 16/10/24.
+//
+
+import UIKit
+
+class CarouselView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
